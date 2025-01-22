2d52007439980d091f2e501b4485a01f6e6f0d73
bradj <bradj@Bradley.dhcp.wustl.edu>
2025-01-22 14:16:17 -0600

    Studio 1 Finished

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -8,15 +8,18 @@ public class HiOne {
 	 * Says hello to the supplied argument
 	 */
 	public static void main(String[] args) {
+		System.out.println("What's your name");
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
 		String name = in.nextLine();
+		System.out.print("Hi " + name + " how are you? ");
+		
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
 		// if the value of name is "Pat"
 		//
+		
 
 	}
 
